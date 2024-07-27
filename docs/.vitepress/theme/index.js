82fa0662e6cd5f5d1aa2ca23bfeb3aeb91e79b65
何方 <he529564582@163.com>
2024-07-28 01:36:31 +0800

    fear:新增nprogress&修改默认样式

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,7 +4,8 @@ import DefaultTheme from 'vitepress/theme'
 import { onMounted, watch, nextTick } from 'vue'
 import { useRoute } from 'vitepress'
 import mediumZoom from 'medium-zoom'
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import './index.css'
 
 export default {
@@ -14,14 +15,27 @@ export default {
     const route = useRoute()
     const initZoom = () => {
       // new mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }) // Should there be a new?
-      new mediumZoom('.main img', { background: 'rgba(0, 0, 0, 0.6)' }) // 图片后面的背景色
+      new mediumZoom('.main img', { background: 'rgba(0, 0, 0, 0.9)' }) // 图片后面的背景色
     }
     onMounted(() => {
       initZoom()
     })
     watch(
       () => route.path,
-      () => nextTick(() => initZoom())
+      (newPath, oldPath) => {
+        // 初始化图片缩放效果
+        nextTick(() => {
+          initZoom()
+        })
+
+        // 监听路由变化以启动和停止 nprogress
+        if (newPath !== oldPath) {
+          NProgress.start()
+        }
+        nextTick(() => {
+          NProgress.done()
+        })
+      }
     )
   }
 }
