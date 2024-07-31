6703cb4e774811f70ddf976c31d87bf3d76423b6
何方 <he529564582@163.com>
2024-07-31 17:53:21 +0800

    update:删除通过脚本产生的本地分支gh-pages

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,11 @@ cd docs/.vitepress/dist
 echo 'www.hefang.site' > CNAME
 
 git init
+# 添加远程分支
 git remote add origin "https://github.com/hefangs/coding.git"
+# 获取远程分支的最新状态
 git fetch origin gh-pages
+# 将远程分支的内容应用到当前分支
 git reset --soft origin/gh-pages
 
 git add -A
@@ -22,6 +25,9 @@ CURRENT_DATE=$(date +"%Y-%m-%d_%H:%M:%S")
 COMMIT_MESSAGE="deploy.sh-$CURRENT_DATE"
 git commit -m "$COMMIT_MESSAGE"
 git push -f origin HEAD:gh-pages
+
+# 删除本地 gh-pages 分支
+git branch -D gh-pages || true
 
 cd -
 rm -rf docs/.vitepress/dist
