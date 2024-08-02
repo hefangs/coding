db74232247e5142a7531381ca50baeed7ec2e5e8
何方 <he529564582@163.com>
2024-08-02 17:27:37 +0800

    update:mediumZoom是函数不是构造函数无需new

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 // .vitepress/theme/index.js
 
 import DefaultTheme from 'vitepress/theme'
-import { onMounted, watch, nextTick, onUnmounted } from 'vue'
+import { onMounted, watch, nextTick } from 'vue'
 import { useRoute } from 'vitepress'
 import mediumZoom from 'medium-zoom'
 import NProgress from 'nprogress'
@@ -16,7 +16,7 @@ export default {
     const route = useRoute()
     const initZoom = () => {
       // new mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }) // Should there be a new?
-      new mediumZoom('.main img', { background: 'rgba(0, 0, 0, 0.9)' }) // 图片后面的背景色
+      mediumZoom('.main img', { background: 'rgba(0, 0, 0, 0.9)' }) // 图片后面的背景色
     }
     onMounted(() => {
       initZoom()
