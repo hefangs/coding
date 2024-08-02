a6511fccbaf17910b57dd183fd0ff9559d8d1796
何方 <he529564582@163.com>
2024-08-02 12:35:34 +0800

    update:nav

--- docs/.vitepress/utils/nav.ts
+++ docs/.vitepress/utils/nav.ts
@@ -1,10 +1,34 @@
 export default function nav() {
   const items = [
-    { text: 'Start', link: '/post/note/start' },
-    { text: 'Sql', link: '/post/note/sql' },
-    { text: 'Appium', link: '/post/note/appium' },
-    { text: 'Jmeter', link: '/post/note/jmeter' },
-    { text: 'Postman', link: '/post/note/postman' }
+    { text: 'Home', link: '/' },
+    {
+      text: 'Tools',
+      items: [
+        { text: 'Git', link: '/post/tools/git' },
+        { text: 'N', link: '/post/tools/n' },
+        { text: 'Nvm', link: '/post/tools/nvm' },
+        { text: 'Nrm', link: '/post/tools/nrm' },
+        { text: 'Pyenv', link: '/post/tools/pyenv' },
+        { text: 'Homebrew', link: '/post/tools/homebrew' },
+        { text: 'Vitepress', link: '/post/tools/vitepress' }
+      ]
+    },
+    {
+      text: 'Test',
+      items: [
+        { text: 'Pytest', link: '/post/test/pytest' },
+        { text: 'Jmeter', link: '/post/test/jmeter' },
+        { text: 'Unittest', link: '/post/test/unittest' },
+        { text: 'Appium', link: '/post/test/appium' },
+        { text: 'Postman', link: '/post/test/postman' },
+        { text: 'Selenium', link: '/post/test/selenium' }
+      ]
+    },
+    {
+      text: 'Javascript',
+      link: '/post/frontend/javascript',
+      activeMatch: '/post/'
+    }
     // { text: 'Tools', link: '/post/note/tools', activeMatch: '/post/note/' },
     // { text: '指南', link: '/post/note/tools', activeMatch: '/post/note/' }
   ]
